28886fea2fd93825bf9da1065733bc0592613dd3
3DBot <jeje.baudrin@gmail.com>
2018-06-21 02:31:13 +0200

    Languages update

--- src/net/faru/api/bungee/commands/MAINTENANCECommand.java
+++ src/net/faru/api/bungee/commands/MAINTENANCECommand.java
@@ -4,10 +4,7 @@ import net.faru.api.bungee.BungeeFaruAPI;
 import net.faru.api.player.FaruPlayer;
 import net.faru.api.player.languages.Lang;
 import net.faru.api.player.rank.Rank;
-import net.faru.api.tools.messages.PluginMessages;
-import net.faru.api.tools.messages.Prefix;
 import net.faru.data.database.servers.IMaintenance;
-import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
@@ -27,30 +24,31 @@ public class MAINTENANCECommand extends Command {
 	public void execute(CommandSender sender, String[] args) {
 		ProxiedPlayer player = (ProxiedPlayer) sender;
 		FaruPlayer faruPlayer = FaruPlayer.getPlayer(player.getUniqueId());
+		Lang lang = faruPlayer.getLanguage();
 		
-		if(faruPlayer.getRank().getPower() < Rank.DEVELOPER.getPower()) { player.sendMessage(PluginMessages.NO_PERMISSION.getMessage()); return; }
+		if(faruPlayer.getRank().getPower() < Rank.DEVELOPER.getPower()) { player.sendMessage(Lang.ERROR.in(lang) + "\n" + Lang.NO_PERMISSION_MESSAGE.in(lang)); return; }
 		if(args == null || args.length > 2 || args[0].equalsIgnoreCase("help")) { player.sendMessage(this.HELP()); return; }
 		
 		if(args[0].equalsIgnoreCase("set")) {
 			if(args[1].equalsIgnoreCase("true")) {
-				if(BungeeFaruAPI.isMaintenance()) { player.sendMessage(PluginMessages.MAINTENANCE_ALREADY_ENABLED.getMessage()); return; }
+				if(BungeeFaruAPI.isMaintenance()) { player.sendMessage(Lang.ERROR.in(lang) + "\n" + Lang.MAINTENANCE_ALREADY_ENABLED.in(lang)); return; }
 				IMaintenance.setState(true);
-				player.sendMessage(Prefix.MAINTENANCE.getPrefix() + ChatColor.GRAY + "Maintenance is now " + ChatColor.GREEN + "enabled" + ChatColor.GRAY + ".");
+				player.sendMessage(Lang.MAINTENANCE_ENABLE.in(lang));
 				return;
 			}
-			if(!BungeeFaruAPI.isMaintenance()) { player.sendMessage(PluginMessages.MAINTENANCE_ALREADY_DISABLED.getMessage()); return; }
+			if(!BungeeFaruAPI.isMaintenance()) { player.sendMessage(Lang.ERROR.in(lang) + "\n" + Lang.MAINTENANCE_ALREADY_DISABLED.in(lang)); return; }
 			IMaintenance.setState(false);
-			player.sendMessage(Prefix.MAINTENANCE.getPrefix() + ChatColor.GRAY + "Maintenance is now " + ChatColor.RED + "disabled" + ChatColor.GRAY + ".");
+			player.sendMessage(Lang.MAINTENANCE_DISABLE.in(lang));
 			return;
 		}
 		
 		if(args[0].equalsIgnoreCase("add")) {
-			player.sendMessage(PluginMessages.FONCTION_IN_WORK.getMessage());
+			player.sendMessage(Lang.WORKING_FEATURE.in(lang));
 			return;
 		}
 		
 		if(args[0].equalsIgnoreCase("remove")) {
-			player.sendMessage(PluginMessages.FONCTION_IN_WORK.getMessage());
+			player.sendMessage(Lang.WORKING_FEATURE.in(lang));
 			return;
 		}
 		
--- src/net/faru/api/player/languages/Lang.java
+++ src/net/faru/api/player/languages/Lang.java
@@ -1,15 +1,73 @@
 package net.faru.api.player.languages;
 
-public enum Lang {
+import org.bukkit.ChatColor;
 
-	FRENCH("French", "Français", "Französisch", "Francés"),
-	ENGLISH("English", "Anglais", "Englisch", "Inglés"),
-	GERMAN("German", "Allemand", "Deutsche", "Alemán"),
-	SPANISH("Spanish", "Espagnol", "Spanisch", "Español"),
+public enum Lang {
 	
-	JOIN_MESSAGE("", "", "", ""),
+	/*  ========== LANGUAGES ==========  */
+	FRENCH("French", "Français", "", ""),
+	ENGLISH("English", "Anglais", "", ""),
+	GERMAN("German", "Allemand", "", ""),
+	SPANISH("Spanish", "Espagnol", "", ""),
+	/* ================================ */
 	
-	ERROR_MESSAGE("", "", "", "");
+	
+	/*  ========== TYPES ==========  */
+	ERROR(ChatColor.DARK_RED + "" + ChatColor.BOLD + "✘" + ChatColor.WHITE + "" + ChatColor.BOLD + " ∣ " + ChatColor.RED + "" + ChatColor.BOLD + "Error :",
+			ChatColor.DARK_RED + "" + ChatColor.BOLD + "✘" + ChatColor.WHITE + "" + ChatColor.BOLD + " ∣ " + ChatColor.RED + "" + ChatColor.BOLD + "Erreur :",
+			"",
+			""),
+	/* ============================== */
+	
+	
+	/*  ========== GENERAL ==========  */
+	NO_PERMISSION_MESSAGE(ChatColor.RED + "You do not have the permission.",
+			ChatColor.RED + "Vous n'avez pas la permission.",
+			"",
+			""),
+	
+	ERROR_HAS_OCCURRED(ChatColor.RED + "An error has occurred.",
+			ChatColor.RED + "Une erreur est survenue.",
+			"",
+			""),
+	/* ============================== */
+	
+	
+	/*  ========== MAINTENANCE ==========  */
+	WORKING_FEATURE(ChatColor.RED + "This feature is in work.",
+			ChatColor.RED + "Cette fonctionnalité est en développement.",
+			"",
+			""),
+	/* ============================== */
+	
+	
+	/*  ========== MAINTENANCE ==========  */
+	MAINTENANCE_ENABLE(ChatColor.GRAY + "The maintenance is now " + ChatColor.GREEN + "enabled" + ChatColor.GRAY + ".",
+			ChatColor.GRAY + "La maintenance est maintenant " + ChatColor.GREEN + "activée" + ChatColor.GRAY + ".",
+			"",
+			""),
+	
+	MAINTENANCE_DISABLE(ChatColor.GRAY + "The maintenance is now " + ChatColor.RED + "disabled" + ChatColor.GRAY + ".",
+			ChatColor.GRAY + "La maintenance est maintenant " + ChatColor.RED + "désactivée" + ChatColor.GRAY + ".",
+			"",
+			""),
+	
+	MAINTENANCE_ALREADY_ENABLED(ChatColor.RED + "The maintenance is already enabled.",
+			ChatColor.RED + "La maintenance est déjà activée.",
+			"",
+			""),
+	
+	MAINTENANCE_ALREADY_DISABLED(ChatColor.RED + "The maintenance is already disabled.",
+			ChatColor.RED + "La maintenance est déjà désactivée.",
+			"",
+			""),
+	/* ============================== */
+	
+	
+	/*  ========== JOIN ==========  */
+	JOIN_MESSAGE(ChatColor.GRAY + " has joined the Hub !", ChatColor.GRAY + " a rejoint le Hub !", "", ""),
+	GAME_JOIN_MESSAGE(ChatColor.GRAY + " has joined the game !", ChatColor.GRAY + " a rejoint la partie !", "", "");
+	/* =========================== */
 		
 	public String english, french, german, spanish;
 	
@@ -25,17 +83,17 @@ public enum Lang {
 	}
 	
 	public String in(Lang language) {
+		String message = this.english;
 		switch(language) {
 			case FRENCH:
-				return this.french;
-			case ENGLISH:
-				return this.english;
+				message = this.french;
 			case GERMAN:
-				return this.german;
+				message = this.german;
 			case SPANISH:
-				return this.spanish;
+				message = this.spanish;
 			default:
-				return this.english;
+				break;
 		}
+		return message + ChatColor.RESET;
 	}
 }
