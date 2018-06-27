a99bbe6a7c5a660ab0d00cacf2235ee41d48f568
3DBot <jeje.baudrin@gmail.com>
2018-06-28 01:05:40 +0200

    update

--- src/net/faru/api/bungee/commands/general/RANKCommand.java
+++ src/net/faru/api/bungee/commands/general/RANKCommand.java
@@ -1,11 +1,10 @@
 package net.faru.api.bungee.commands.general;
 
-import org.bukkit.entity.Player;
-
 import net.faru.api.bungee.proxiedplayer.FaruBungeePlayer;
 import net.faru.api.spigot.player.languages.Lang;
 import net.faru.api.spigot.player.rank.Rank;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class RANKCommand extends Command {
@@ -17,7 +16,7 @@ public class RANKCommand extends Command {
 	@SuppressWarnings("deprecation")
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		FaruBungeePlayer faruPlayer = FaruBungeePlayer.getPlayer(((Player) sender).getUniqueId());
+		FaruBungeePlayer faruPlayer = FaruBungeePlayer.getPlayer(((ProxiedPlayer) sender).getUniqueId());
 		Lang lang = faruPlayer.getLanguage();
 		
 		if(faruPlayer.getRank().getPower() < Rank.ADMINISTRATOR.getPower()) { faruPlayer.getPlayer().sendMessage(Lang.ERROR.in(lang) + "\n" + Lang.NO_PERMISSION_MESSAGE.in(lang)); return; }
@@ -28,10 +27,14 @@ public class RANKCommand extends Command {
 			for(Rank rank : Rank.values()) {
 				ranks = ranks + " §7» §f" + rank.getOrder() + " §8/ §3" + rank.getPower() + " §8/ " + rank.getChatColor() + rank.getIdName() + " §8/ " + rank.getColor() + rank.getPrefix() + "\n";
 			}
-			faruPlayer.getPlayer().sendMessage("\n"
-												+ "§7Liste des grades :\n"
-												+ "\n"
-												+ ranks != "" ? ranks : "§cAucun grade disponible.\n");
+			faruPlayer.getPlayer().sendMessage("");
+			faruPlayer.getPlayer().sendMessage("§7Liste des grades ");
+			faruPlayer.getPlayer().sendMessage("");
+			faruPlayer.getPlayer().sendMessage(ranks != "" ? ranks : "§cAucun grade disponible.");
+		}
+		
+		if(args[0].equalsIgnoreCase("reset")) {
+			
 		}
 		
 		else {
