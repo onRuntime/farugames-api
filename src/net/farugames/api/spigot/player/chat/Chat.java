5454359c7b6098ef3bc4372ab453511b59831c12
BuildTools <unconfigured@null.spigotmc.org>
2018-07-12 01:41:32 +0200

    Update 0.5.6 by ChocoIG (Chat)

--- src/net/farugames/api/spigot/player/chat/Chat.java
+++ src/net/farugames/api/spigot/player/chat/Chat.java
@@ -1,5 +1,48 @@
 package net.farugames.api.spigot.player.chat;
 
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import net.farugames.api.spigot.player.FaruPlayer;
+import net.farugames.api.spigot.player.data.DataType;
+import net.farugames.api.spigot.player.rank.Rank;
+
 public class Chat {
+	
+	public static void customMessage(AsyncPlayerChatEvent e, String prefix, String suffix) {
+		Player sender = e.getPlayer();
+		FaruPlayer faruSender = FaruPlayer.getPlayer(sender.getUniqueId());
+		Player fs = faruSender.getPlayer();
+		Rank r = faruSender.getRank();
+		String msg = e.getMessage();
+
+		e.setCancelled(true);
+
+		if ((boolean) faruSender.getData(DataType.ALLOW_CHAT) == true) {
+			for (Player op : e.getRecipients()) {
+				FaruPlayer faruOp = FaruPlayer.getPlayer(op.getUniqueId());
+				Player fOp = faruOp.getPlayer();
+				if (msg.contains(fOp.getName())) {
+					if ((boolean) faruOp.getData(DataType.ALLOW_CHAT_MENTIONS).equals(true)) {
+						fOp.playSound(fOp.getLocation(), Sound.BLOCK_NOTE_PLING, 10, 1);
+						// TitleManager.sendActionBar(fOp, "§a" + fs.getName() +
+						// "§d vous a mentionné dans le chat !");
+						msg = msg.replaceAll(fOp.getName(), "§e@" + fOp.getName() + r.getChatColor());
+					}
+				}
+			}
+			msg = msg.replaceAll("@", "");
+			for (Player op : e.getRecipients()) {
+				FaruPlayer faruOp = FaruPlayer.getPlayer(op.getUniqueId());
+				if ((boolean) faruOp.getData(DataType.ALLOW_CHAT).equals(true)) {
+					op.sendMessage(prefix + r.getColor() + r.getPrefix() + " " + fs.getName() + suffix + " §8»§r "
+							+ r.getChatColor() + msg);
+				}
+			}
+		} else {
+			// errormsg
+		}
+	}
 
 }
--- src/net/farugames/api/tools/builders/title/ActionBar.java
+++ src/net/farugames/api/tools/builders/title/ActionBar.java
@@ -13,53 +13,52 @@ import net.minecraft.server.v1_9_R2.Packet;
 import net.minecraft.server.v1_9_R2.PacketPlayOutChat;
 import net.minecraft.server.v1_9_R2.PlayerConnection;
 
-public class ActionBar
-{
-    private String message;
-    private int stay;
-    
-    public String getMessage() {
-        return this.message;
-    }
-    
-    public int getStay() {
-        return this.stay;
-    }
-    
-    public void ActionBarBuilder(final String message) {
-        this.message = message;
-    }
-    
-    public ActionBar withStay(final int stay) {
-        this.stay = stay;
-        return this;
-    }
-    
-    private IChatBaseComponent buildChatComponent(final String msg) {
-        return IChatBaseComponent.ChatSerializer.a("{'text':'" + msg + "'}");
-    }
-    
-    public void sendTo(final Collection<Player> players) {
-        players.forEach(this::sendTo);
-    }
-    
-    @SuppressWarnings({ "null", "rawtypes" })
+public class ActionBar {
+	private String message;
+	private int stay;
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public int getStay() {
+		return this.stay;
+	}
+
+	public void ActionBarBuilder(final String message) {
+		this.message = message;
+	}
+
+	public ActionBar withStay(final int stay) {
+		this.stay = stay;
+		return this;
+	}
+
+	private IChatBaseComponent buildChatComponent(final String msg) {
+		return IChatBaseComponent.ChatSerializer.a("{'text':'" + msg + "'}");
+	}
+
+	public void sendTo(final Collection<Player> players) {
+		players.forEach(this::sendTo);
+	}
+
+	@SuppressWarnings({ "null", "rawtypes" })
 	public void sendTo(final Player player) {
-        final PlayerConnection connection = ((CraftPlayer)player).getHandle().playerConnection;
-        final PacketPlayOutChat packet = new PacketPlayOutChat(this.buildChatComponent(this.message), (byte)2);
-        connection.sendPacket((Packet)packet);
-        if (this.stay != 0) {
-            final PlayerConnection playerConnection = null;
-            Bukkit.getScheduler().runTaskLater((Plugin) Main.getInstance(), () -> {
-            	final PacketPlayOutChat clear = new PacketPlayOutChat(this.buildChatComponent(""), (byte)2);
-                playerConnection.sendPacket((Packet)clear);
-            }, this.stay * 20L);
-        }
-    }
-    
-    public void sendToServer() {
-        for (final Player player : Bukkit.getOnlinePlayers()) {
-            this.sendTo(player);
-        }
-    }
+		final PlayerConnection connection = ((CraftPlayer) player).getHandle().playerConnection;
+		final PacketPlayOutChat packet = new PacketPlayOutChat(this.buildChatComponent(this.message), (byte) 2);
+		connection.sendPacket((Packet) packet);
+		if (this.stay != 0) {
+			final PlayerConnection playerConnection = null;
+			Bukkit.getScheduler().runTaskLater((Plugin) Main.getInstance(), () -> {
+				final PacketPlayOutChat clear = new PacketPlayOutChat(this.buildChatComponent(""), (byte) 2);
+				playerConnection.sendPacket((Packet) clear);
+			}, this.stay * 20L);
+		}
+	}
+
+	public void sendToServer() {
+		for (final Player player : Bukkit.getOnlinePlayers()) {
+			this.sendTo(player);
+		}
+	}
 }
